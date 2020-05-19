bc200776107195bd35ad6ac5f3c552c7d2448758
Sebastian Warnke <seb@Warnke.fritz.box>
2020-05-19 19:53:48 +0200

    first commit

---
+++ crossmarks-server/src/main/java/com/sebwarnke/crossmarks/crossmarksserver/CrossmarksServerApplication.java
@@ -0,0 +1,13 @@
+package com.sebwarnke.crossmarks.crossmarksserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossmarksServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossmarksServerApplication.class, args);
+	}
+
+}
---
+++ crossmarks-server/src/test/java/com/sebwarnke/crossmarks/crossmarksserver/CrossmarksServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebwarnke.crossmarks.crossmarksserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossmarksServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
