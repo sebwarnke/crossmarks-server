1c5bf3b460cf5cfa1b8e671541f6c04b5d396302
Sebastian Warnke <sw-github@sebwarnke.com>
2020-07-29 22:17:37 +0200

    test(service): add yet empty test for bookmarkservice

---
+++ src/test/java/com/sebwarnke/crossmarks/crossmarksserver/core/services/BookmarkServiceTest.java
@@ -0,0 +1,11 @@
+package com.sebwarnke.crossmarks.crossmarksserver.core.services;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookmarkServiceTest {
+
+}
