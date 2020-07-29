8246923f0b4b7f716e243dd541771039307fd736
Sebastian Warnke <sw-github@sebwarnke.com>
2020-07-29 18:33:11 +0200

    feat(logging): add slf4j and logback incl. config

--- src/main/java/com/sebwarnke/crossmarks/crossmarksserver/core/api/BookmarkController.java
+++ src/main/java/com/sebwarnke/crossmarks/crossmarksserver/core/api/BookmarkController.java
@@ -1,9 +1,11 @@
 package com.sebwarnke.crossmarks.crossmarksserver.core.api;
 
-import com.sebwarnke.crossmarks.crossmarksserver.core.execptions.InvalidUrlException;
-import com.sebwarnke.crossmarks.crossmarksserver.core.execptions.NoSuchBookmarkException;
+import com.sebwarnke.crossmarks.crossmarksserver.core.exceptions.InvalidUrlException;
+import com.sebwarnke.crossmarks.crossmarksserver.core.exceptions.NoSuchBookmarkException;
 import com.sebwarnke.crossmarks.crossmarksserver.core.model.entities.Bookmark;
 import com.sebwarnke.crossmarks.crossmarksserver.core.services.BookmarkService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,7 @@ import java.util.Collection;
 @RestController
 @RequestMapping("/api")
 public class BookmarkController {
+  public static final Logger log = LoggerFactory.getLogger(BookmarkController.class);
 
   private BookmarkService bookmarkService;
 
@@ -30,30 +33,38 @@ public class BookmarkController {
 
   @GetMapping("/bookmark")
   public Collection<Bookmark> getAllBookmarks() {
+    log.debug("Endpoint called: GET /bookmark");
     return bookmarkService.getAllBookmarks();
   }
 
   @GetMapping("/bookmark/{id}")
   public ResponseEntity<?> getBookmarkById(@PathVariable String id) {
+    log.debug("Endpoint called: GET /bookmark/{}", id);
     try {
+      log.debug("-- Entity found");
       return ResponseEntity.ok().body(bookmarkService.getBookmark(id));
 
     } catch (NoSuchBookmarkException e) {
+      log.debug("-- Entity not found");
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"message\" : \"" + e.getMessage() + "\"}");
     }
   }
 
   @DeleteMapping("/bookmark/{id}")
   public void deleteBookmark(@PathVariable String id) {
+    log.debug("Endpoint called: DELETE /bookmark/{}", id);
     bookmarkService.deleteBookmark(id);
   }
 
   @PostMapping("/bookmark")
   public ResponseEntity<?> createBookmark(@RequestBody Bookmark bookmarkTemplate) {
+    log.debug("Endpoint called: POST /bookmark");
     try {
+      log.debug("-- Entity created");
       return ResponseEntity.ok().body(bookmarkService.createBookmark(bookmarkTemplate));
 
     } catch (InvalidUrlException e) {
+      log.debug("Entity not created; {}", e.getMessage());
       return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("{\"message\" : \"" + e.getMessage() + "\"}");
     }
   }
