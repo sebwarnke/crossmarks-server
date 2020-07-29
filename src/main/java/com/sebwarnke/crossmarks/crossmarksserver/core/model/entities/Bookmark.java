084746936296647642a16db130ed4ba2086b07f7
Sebastian Warnke <sw-github@sebwarnke.com>
2020-07-29 21:22:18 +0200

    feat(logging): add logging to Bookmark model

--- src/main/java/com/sebwarnke/crossmarks/crossmarksserver/core/model/entities/Bookmark.java
+++ src/main/java/com/sebwarnke/crossmarks/crossmarksserver/core/model/entities/Bookmark.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
